d18029ba071aa3ff7e6be0d30f1c1ddd9da88724
Vysakh P <vysakhp17@gmail.com>
2022-10-24 14:38:03 +0530

    order, view product shop

--- Shop/jailSelect.php
+++ Shop/jailSelect.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Methods:GET,POST");
 header("Access-Control-Allow-Headers:*");
 $con=mysqli_connect("127.0.0.1","root","","db_exconvict");
 $Datalist=array();
-$query=mysqli_query( $con,"SELECT * FROM tbl_production r INNER JOIN tbl_product p ON r.product_id = p.product_id");
+$query=mysqli_query( $con,"SELECT * FROM tbl_district d INNER JOIN tbl_place p INNER JOIN tbl_centraljail c ON d.district_id=p.district_id AND p.place_id=c.place_id");
 while($row = mysqli_fetch_assoc($query))
 {
 array_push($Datalist,$row);
--- Shop/viewproduct.php
+++ Shop/viewproduct.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Methods:GET,POST");
 header("Access-Control-Allow-Headers:*");
 $con=mysqli_connect("127.0.0.1","root","","db_exconvict");
 $Datalist=array();
-$query=mysqli_query( $con,"SELECT * FROM tbl_production r INNER JOIN tbl_product p ON r.product_id = p.product_id");
+$query=mysqli_query( $con,"SELECT * FROM tbl_product p INNER JOIN tbl_productrate r ON p.product_id=r.product_id INNER JOIN tbl_centraljail c ON c.centraljail_id=r.centraljail_id");
 while($row = mysqli_fetch_assoc($query))
 {
 array_push($Datalist,$row);
